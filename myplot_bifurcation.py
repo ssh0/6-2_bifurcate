e28231f19fe0435039535270fb838d2b0670beab
Shotaro Fujimoto <fuji101ijuf@gmail.com>
2015-03-01 18:20:06 +0900

    modify

--- myplot_bifurcation.py
+++ myplot_bifurcation.py
@@ -6,15 +6,14 @@
 Shotaro Fujimoto
 """
 import matplotlib.pylab as plt
-import array as array
 import numpy as np
 
 def Plot(func, x0, ntransient, nplot, r0, rmax, dr):
     global x
-    count=int((rmax-r0)/dr)
-    [_Plot(func, r0+dr*n, x0, ntransient, nplot) for n in range(count+1)]
-    plt.gca().set_xlim(r0,rmax)
-    plt.gca().set_ylim(min(x),max(x))
+    count = int((rmax-r0)/dr)
+    fig = [_Plot(func, r0+dr*n, x0, ntransient, nplot) for n in range(count+1)]
+    plt.gca().set_xlim(r0, rmax)
+    plt.gca().set_ylim(np.min(x), np.max(x))
     plt.xlabel(r'$r$', fontsize=16)
     plt.ylabel(r'$x$', fontsize=16)
     plt.title('Bifurcation Diagram')
@@ -22,11 +21,11 @@ def Plot(func, x0, ntransient, nplot, r0, rmax, dr):
 
 def _Plot(function, r, x0, ntransient, nplot):
     global x
-    n=ntransient+nplot*2
-    x=array.array('f')
-    x.append(x0)
-    for i in range(n):
+    n = ntransient + nplot*2
+    x = [x0]
+    for i in np.arange(n):
         x.append(function(x[i], r))
+    x = np.array(x)
     plt.scatter([r]*nplot, x[ntransient+1:ntransient+nplot+1],
                 color='r', s=0.1, marker='.'
                )
